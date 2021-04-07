be1195c0c15f10bcdb596a853b4f7efb20fb331d
kouki <hinoatarusakamichi@gmail.com>
2021-04-07 22:17:59 +0900

    java追加

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,5 @@ set PATH /opt/homebrew/bin $PATH
 
 set -x PATH $HOME/.anyenv/bin $PATH
 eval (anyenv init - | source)
+set -g fish_user_paths "/opt/homebrew/opt/openjdk/bin" $fish_user_paths
+set -g fish_user_paths "/opt/homebrew/opt/openjdk@11/bin" $fish_user_paths
